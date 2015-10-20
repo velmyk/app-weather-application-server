d23375fbcdabba51298cb81f8e48142d75b749e7
velmyk <velmyk@gmail.com>
2015-10-20 15:12:09 +0300

    Deleted unneccesary code

--- config/env/index.js
+++ config/env/index.js
@@ -3,7 +3,5 @@
 module.exports = {
   mongo: {
     uri: 'mongodb://citydb:111111@ds051853.mongolab.com:51853/cities'
-    // uri: 'mongodb://10.17.164.168:27017/weather-app-db'
-    // uri: 'mongodb://localhost:27017/weather-app-db'
   }
 };
