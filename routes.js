ce60713d6a2bfcd31d001f4859d4695ef77cb6ce
velmyk <velmyk@gmail.com>
2015-10-20 12:12:49 +0300

    Add package.json

--- routes.js
+++ routes.js
@@ -1,22 +1,4 @@
-var	weather = require("./data/weather");
-
-
 module.exports = function(app) {
-
-	app.get('/api/weather', function (req, res){
-		var statusCode = 404;
-			resData = {"error": "city not found"};
-
-		weather.forEach( function (item, i ) {
-			if (item.city.id == req.query.id) {
-				statusCode = 200;
-				resData = item;
-				return;
-			}
-		});
-
-		res.status(statusCode).json(resData);
-	});
 
 	app.use('/api/city', require("./api/city"));
 
